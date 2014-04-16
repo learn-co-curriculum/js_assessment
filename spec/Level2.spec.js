de39f9c1fa4c7a71920b35e3d2f647533bbdfd4e
Steven Nunez <steven.nunez@gmail.com>
2014-04-16 18:03:20 -0400

    Solve for level 2

--- spec/Level2.spec.js
+++ spec/Level2.spec.js
@@ -2,32 +2,41 @@
 
 describe('Level2', function() {
 	describe('Objects', function(){
-		it("adds a new key", function(){
+		it("adds a new property", function(){
 			var instructor = { name: "Steven", age: 30 };
        // Add a property location with a value of "NYC" to instructor
+       instructor.location = "NYC";
        expect(instructor.location).toEqual("NYC");
      });
 
-		it("prints out all the key value pairs in the hash", function(){
+		it("prints out all the property value pairs in the object", function(){
 			var instructor = { name: "Steven", age: 30 };
 			spyOn(console, 'log');
-
-      // Iterate over the hash and print each key value pair. 
+      // Iterate over the object and print each property value pair. 
       // Print using string interpolation to match the expectations.			
-      expect(console.log).toHaveBeenCalledWith("Key is name. Value is Steven");
-      expect(console.log).toHaveBeenCalledWith("Key is age. Value is 30");
+    for(var property in instructor){
+    	console.log("Property is " + property + ". Value is " + instructor[property]);
+    }
+      expect(console.log).toHaveBeenCalledWith("Property is name. Value is Steven");
+      expect(console.log).toHaveBeenCalledWith("Property is age. Value is 30");
     });
 
-		it("returns a value from the hash", function(){
+		it("returns a value from the object", function(){
 			var instructor = {name: "Steven", age: 30};
-			var name = "banana"; // extract 'name' value from the object
+			var name = instructor.name; // extract 'name' value from the object
 			expect(name).toEqual("Steven");
 		});
 
-		it("finds a key from a known value", function(){
+		it("finds a property from a known value", function(){
 			var instructor = {name: "Steven", age: 30};
-			var key_from_value = "banana"; // write code that finds the property name where the value is 30
-			expect(key_from_value).toEqual('age');
+			var property_from_value;
+			for(var prop in instructor){
+				if(instructor[prop] == 30){
+					property_from_value = prop;
+				}
+			}
+			 // write code that finds the property name where the value is 30
+			expect(property_from_value).toEqual('age');
 		});
 	});
 });
